b4bb9f05d808cdf5ff3913adab9871f1578fb2a8
Wladimir Kusnezow <wladimir.kusnezow@arcor.de>
2013-03-22 12:02:34 +0100

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WsfinanzPrepage::Application.config.secret_token = 'a332b713adad11690281e49d79194a7f182d62084aa72b32d65c1d0abbb93fb79a5e8b817252e7a03439dd6728dc662fb11e526e30ebfc9d4ca16a347151ecbd'
